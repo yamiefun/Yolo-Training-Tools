9132efe69c811d05938bbe80446b2764162f12c9
yamiefun <yamiefun@gmail.com>
2022-03-11 16:25:00 +0800

    1. Add mapping. 2. Add bbox validity test.

--- modify_label.py
+++ modify_label.py
@@ -1,6 +1,13 @@
 import argparse
 import os
 
+MAPPING = {'0': '0',
+           '1': '1',
+           '2': '2',
+           '3': '3',
+           '4': '4',
+           '5': '5'}
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -8,21 +15,29 @@ def parse_args():
     return parser.parse_args()
 
 
+def check_valid_bbox(line):
+    info = line.split()
+    if len(info) < 5:
+        return False
+    for idx in range(1, 5):
+        if float(info[idx]) == 0:
+            return False
+    return True
+
+
 def main() -> None:
     args = parse_args()
-    label_list = [
-        os.path.join(args.folder_path, filename)
-        for filename in os.listdir(args.folder_path)
-        if filename[-4:] == ".txt"
-    ]
-    for label_file in label_list:
+    for filename in os.listdir(args.folder_path):
+        if filename[-4:] != ".txt":
+            continue
+        label_file = os.path.join(args.folder_path, filename)
         with open(label_file, "r") as f:
             lines = f.readlines()
         with open(label_file, "w") as f:
             for line in lines:
-                newline = '1' if line[0] == '0' else '0'
-                newline += line[1:]
-                f.write(newline)
+                if check_valid_bbox(line):
+                    newline = MAPPING[line[0]] + line[1:]
+                    f.write(newline)
 
 
 if __name__ == "__main__":
